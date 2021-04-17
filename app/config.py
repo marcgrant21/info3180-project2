65cc38e01efffe0bb0d71afc9789186ee1798c5d
marcgrant21 <marcgrant10@gmail.com>
2021-04-17 11:07:13 -0500

    project set up

---
+++ app/config.py
@@ -0,0 +1,16 @@
+import os
+
+class Config(object):
+    """Base Config Object"""
+    DEBUG = False
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
+    UPLOAD_FOLDER = os.environ['UPLOAD_FOLDER'] = './app/static/uploads' 
+
+class DevelopmentConfig(Config):
+    """Development Config that extends the Base Config Object"""
+    DEVELOPMENT = True
+    DEBUG = True
+
+class ProductionConfig(Config):
+    """Production Config that extends the Base Config Object"""
+    DEBUG = False
---
+++ app/static/js/app.js
@@ -0,0 +1,95 @@
+/* Add your Application JavaScript */
+// Instantiate our main Vue Instance
+const app = Vue.createApp({
+    data() {
+        return {
+
+        }
+    }
+});
+
+app.component('app-header', {
+    name: 'AppHeader',
+    template: `
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+      <a class="navbar-brand" href="#">Lab 7</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <router-link class="nav-link" to="/">Home <span class="sr-only">(current)</span></router-link>
+          </li>
+          <li class="nav-item active">
+          <router-link class="nav-link" to="/upload">Upload</router-link>
+        </li>
+        </ul>
+      </div>
+    </nav>
+    `
+});
+
+app.component('app-footer', {
+    name: 'AppFooter',
+    template: `
+    <footer>
+        <div class="container">
+            <p>Copyright &copy; {{ year }} Flask Inc.</p>
+        </div>
+    </footer>
+    `,
+    data() {
+        return {
+            year: (new Date).getFullYear()
+        }
+    }
+});
+
+const Home = {
+    name: 'Home',
+    template: `
+    <div class="jumbotron">
+        <h1>Lab 7</h1>
+        <p class="lead">In this lab we will demonstrate VueJS working with Forms and Form Validation from Flask-WTF.</p>
+    </div>
+    `,
+    data() {
+        return {}
+    }
+};
+
+const NotFound = {
+    name: 'NotFound',
+    template: `
+    <div>
+        <h1>404 - Not Found</h1>
+    </div>
+    `,
+    data() {
+        return {}
+    }
+};
+
+
+
+
+
+// Define Routes
+const routes = [
+    { path: "/", component: Home },
+    // Put other routes here
+
+    // This is a catch all route in case none of the above matches
+    { path: '/:pathMatch(.*)*', name: 'not-found', component: NotFound }
+];
+
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHistory(),
+    routes, // short for `routes: routes`
+});
+
+app.use(router);
+
+app.mount('#app');
