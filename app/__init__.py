371d2eb8b9bc9844cbda660548628d1440c6f866
marcgrant21 <marcgrant10@gmail.com>
2021-04-19 08:08:45 -0500

    update init

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
-from app.config import Config
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from .config import Config
 from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 csrf = CSRFProtect(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
 app.config.from_object(Config)
 
 from app import views
